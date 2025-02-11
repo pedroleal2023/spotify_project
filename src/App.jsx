f379bf61d6b3eef511819245cd3311f641d68b4c
Pedro Miguel <rudaprod.oficial@gmail.com>
2025-02-11 16:14:15 -0300

    Update Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
+import Main from "./components/Main"; // ✅ Importação corrigida
 
 function App() {
   return (
-    <>
+    <Router>
+      {" "}
+      {/* Envolvendo toda a aplicação com Router */}
       <Header />
       <Main />
-    </>
+    </Router>
   );
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,42 @@
 import React from "react";
+import ItemsList from "./ItemsList";
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
 
-const main = () => {
+const Main = ({ type }) => {
+  const itemsPerRow = type
+    ? Infinity
+    : Math.floor((window.innerWidth - 25 * 2 - 8 * 2) / 172);
+
   return (
-    <div className="main">
-      <div className="main__texts">
-        <h2>Artistas populares</h2>
-        <a className="main_link" href="/">
-          Mostrar tudo
-        </a>
-      </div>
-    </div>
+    <section className="main">
+      {!type || type === "artists" ? (
+        <ItemsList
+          title="Artistas populares"
+          type="Artista"
+          path="/artists"
+          individualPath="/artist"
+          itemsToShow={itemsPerRow}
+          itemsArray={artistArray}
+        />
+      ) : (
+        ""
+      )}
+
+      {!type || type === "songs" ? (
+        <ItemsList
+          title="Músicas populares"
+          path="/songs"
+          individualPath="/song"
+          itemsToShow={itemsPerRow * 2}
+          itemsArray={songsArray}
+          style="square"
+        />
+      ) : (
+        ""
+      )}
+    </section>
   );
 };
 
-export default main;
+export default Main;
